16b7cb771ca81813e6bb8526150608af89b5a28b
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-01-09 17:27:15 +0900

    Implement floating-action-buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,113 @@
-//import 'package:flutter/material.dart';
-//
-//void main() => runApp(MyApp());
-//
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Welcome to Flutter',
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('Welcome to Flutter'),
-//        ),
-//        body: Center(
-//          child: Text('Hello World'),
-//        ),
-//      ),
-//    );
-//  }
-//}
-
 import 'package:flutter/material.dart';
 import 'header.dart';
 import 'footer.dart';
 
 void main() {
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Second flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
       home: Scaffold(
         appBar: Header(),
-        body: Center(child: Text("オラオラオラオラ")),
+        body: MyHomePage(title: 'Second Flutter Page'),
         bottomNavigationBar: Footer(),
       ),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+//        verticalDirection: VerticalDirection.up,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              child: FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: _decrementCounter,
+                tooltip: 'Decrement',
+                child: Icon(Icons.exposure_neg_1),
+              ),
+            ),
+            Container(
+//              margin: EdgeInsets.only(bottom: 16.0),
+              child: FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: _resetCounter,
+                tooltip: 'Reset',
+                child: Icon(Icons.exposure_zero),
+              ),
+            ),
+            Container(
+              child: FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: Icon(Icons.exposure_plus_1),
+              ),
+            ),
+          ]
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
